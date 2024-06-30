08e6451b1b08643168cd3b1325ef7b43576a4a9a
VladimirKoryakovtsevI22 <157983129+VladimirKoryakovtsevI22@users.noreply.github.com>
2024-06-30 19:33:09 +0300

    Add files via upload

---
+++ switchh/switchh/switchh.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+	printf("Enter number: ");
+	scanf_s("%d", &a);
+
+	switch (a)
+	{
+		case '1':
+			printf("1\n");
+			break;
+		case '2':
+			printf("2\n");
+			break;
+		case '3':
+			printf("3\n");
+			break;
+		case '4':
+			printf("4\n");
+			break;
+		case '5':
+			printf("5\n");
+			break;
+		default:
+			printf("you entered more than 5\n");
+			break;
+	}
+	
+	return 0;
+}
+
